265fa6f0b6e9d87cef4ad1bd6def62300f3057f6
DarthAsh <54763573+DarthAsh@users.noreply.github.com>
2024-09-26 21:45:28 -0400

    small fix

--- hw3/guess_large.sh
+++ hw3/guess_large.sh
@@ -2,7 +2,7 @@
 #This file runs the_guesser.py on 4 medium and 3 large dimensionality datasets in that order
 
 echo "Running Python script for CSV files..."
-mkdir -p tmp
+mkdir -p tmp2
 python3.13 the_guesser.py "./data/optimize/config/Apache_AllMeasurements.csv" | tee ./tmp2/Apache_AllMeasurements.csv &
 python3.13 the_guesser.py "./data/optimize/config/SS-P.csv" | tee ./tmp2/SS-P.csv &
 python3.13 the_guesser.py "./data/optimize/config/SS-L.csv" | tee ./tmp2/SS-L.csv &
